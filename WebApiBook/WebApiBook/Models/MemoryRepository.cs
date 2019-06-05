04e3496de5d7fb6b636f1811389b47c80c143da1
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-05 09:40:32 +0300

    Update MemoryRepository
    I implement a dictionary and fill it with data;

--- WebApiBook/WebApiBook/Models/MemoryRepository.cs
+++ WebApiBook/WebApiBook/Models/MemoryRepository.cs
@@ -15,6 +15,16 @@ namespace WebApiBook.Models
         public Book this[int id] => throw new NotImplementedException();
 
         private Dictionary<int, Book> items;
+        public MemoryRepository()
+        {
+            items = new Dictionary<int, Book>();
+            new List<Book>
+            {
+               new Book{BookId=1,Name="Lord of the Flies",Author="William Golding",Price=15},
+               new Book{BookId=2,Name="Sherlock Holmes",Author="Sir Arthur Conan Doyle",Price=17},
+               new Book{BookId=3,Name="Gulliver's Travels",Author="Jonathan Swift",Price=12}
+            }.ForEach(r => AddBook(r));
+        }
 
         public IEnumerable<Book> Books => throw new NotImplementedException();
 
