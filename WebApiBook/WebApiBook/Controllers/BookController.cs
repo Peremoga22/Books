a525739877fe9a8830c7a3533116b862af10dddc
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-05 10:08:04 +0300

    Create BookController
    I will set up access IRepository;

---
+++ WebApiBook/WebApiBook/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiBook.Interfaces;
+
+namespace WebApiBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private IRepository repository { get; set; }
+
+        public BookController(IRepository repo) => repository = repo;
+    }
+}
