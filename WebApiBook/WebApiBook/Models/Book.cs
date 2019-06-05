56afe8c931aa16a92a08f60ab44c62ee3cac5f18
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-05 09:19:29 +0300

    Create Book.cs
    Add filds;

---
+++ WebApiBook/WebApiBook/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBook.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+    }
+}
