a1615e4af0f2927cf6d47fad314d7e6b69febf46
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-05 09:28:52 +0300

    Create MemoryRepository
    Implement the interface IRepository;

---
+++ WebApiBook/WebApiBook/Models/MemoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiBook.Interfaces;
+
+namespace WebApiBook.Models
+{
+    public class MemoryRepository : IRepository
+    {
+        public Book this[int id] => throw new NotImplementedException();
+
+        public IEnumerable<Book> Books => throw new NotImplementedException();
+
+        public Book AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
